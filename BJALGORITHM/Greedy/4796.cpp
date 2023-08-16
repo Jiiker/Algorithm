091856e7914b1129220007c7a99783868366db5e
Jiiker <motive46@naver.com>
2023-08-16 17:55:13 +0900

    20230816

---
+++ BJALGORITHM/Greedy/4796.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int testcase = 1;
+
+    while (testcase >= 1)
+    {
+        int period, length, vacation;
+        int cycle, remain, result;
+
+        cin >> length >> period >> vacation;
+
+        if (length == 0 && period == 0 && vacation == 0)
+            return 0;
+
+        cycle = vacation / period;
+        remain = vacation - (cycle * period);
+
+        if (remain >= length)
+            result = length * cycle + length;
+        else
+            result = length * cycle + remain;
+
+        cout << "Case " << testcase << ": " << result << '\n';
+    }
+
+    return 0;
+}
