6a7bb198a0a948e3fbd1e36bd1cba3f25f2c28a5
Jiiker <motive46@naver.com>
2023-09-05 11:16:20 +0900

    20230905

---
+++ BJALGORITHM/Heap/2075.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int N;
+  vector<int> V;
+
+  cin >> N;
+
+  for (int i = 0; i < N; i++)
+  {
+    for (int j = 0; j < N; j++)
+    {
+      int num;
+      cin >> num;
+      V.push_back(num);
+    }
+
+    make_heap(V.begin(), V.end(), greater<>());
+    while (V.size() > i + 1)
+    {
+      pop_heap(V.begin(), V.end(), greater<>());
+      V.pop_back();
+    }
+  }
+
+  make_heap(V.begin(), V.end());
+
+  for (int i = 0; i < N - 1; i++)
+  {
+    pop_heap(V.begin(), V.end());
+    V.pop_back();
+  }
+
+  cout << V.front();
+
+  return 0;
+}
