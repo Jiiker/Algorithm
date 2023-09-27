b66bd3220f0324e3c29c084f16a5c3f29d244889
Jiiker <motive46@naver.com>
2023-09-27 18:52:30 +0900

    20230927

--- BJALGORITHM/DP/11048.cpp
+++ BJALGORITHM/DP/11048.cpp
@@ -6,8 +6,6 @@ int main()
 {
   int N, M;
   int maze[1001][1001] = { 0 };
-  int dx[3] = { 1, 0, 1 };
-  int dy[3] = { 0, 1, 1 };
 
   cin >> N >> M;
   for (int i = 1; i <= N; i++)
@@ -21,7 +19,7 @@ int main()
     for (int j = 1; j <= M; j++)
     {
       if (i != 0 && j != 0)
-        maze[i][j] += max(maze[i - 1][j], maze[i][j - 1]);
+        maze[i][j] += max(max(maze[i - 1][j], maze[i][j - 1]), maze[i][j - 1]);
     }
   }
 
