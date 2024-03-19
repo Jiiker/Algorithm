e4b5e55f9432501c0e771130d0e0753147d3f488
Jiiker <motive46@naver.com>
2024-03-19 11:46:30 +0900

    20240319

---
+++ Programmers/long_jump.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long solution(int n) {
+    long long answer = 0;
+    long long DP[2001] = { 0 };
+
+    DP[1] = 1;
+    DP[2] = 2;
+    
+    for (int i = 3; i <= n; i++)
+    {
+        DP[i] = (DP[i - 1] + DP[i - 2]) % 1234567;
+    }
+    
+    answer = DP[n];
+    
+    return answer;
+}
