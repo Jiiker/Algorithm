8ffb42926351c39c449bd5dd7fd9394f41c37494
Donggi Lim <motive46@naver.com>
2025-02-28 17:33:00 +0900

    [Silver I] Title: 절댓값 힙, Time: 572 ms, Memory: 2804 KB -BaekjoonHub

---
+++ C++17/백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+  int abs_num, num;
+
+  bool operator<(const Node& n) const {
+    if (abs_num == n.abs_num) {
+      return num > n.num;
+    } else {
+      return abs_num > n.abs_num;
+    }
+  }
+};
+
+int main() {
+  int N = 0;
+  cin >> N;
+
+  priority_queue<Node> pq;
+
+  for (int i = 0; i < N; i++) {
+    int num;
+
+    cin >> num;
+
+    if (num != 0) {
+      pq.push({abs(num), num});
+    } else {
+      if (pq.empty()) {
+        cout << 0 << '\n';
+      } else {
+        int temp = pq.top().num;
+        pq.pop();
+        cout << temp << '\n';
+      }
+    }    
+  }
+
+  return 0;
+}
